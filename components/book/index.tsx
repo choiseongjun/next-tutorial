afa775567e0733088ad9fcd0750281f2da12d84c
최성준 <csj2702@naver.com>
2022-04-29 18:28:55 +0900

    update p css ex

--- components/book/index.tsx
+++ components/book/index.tsx
@@ -6,7 +6,7 @@ const index = ({ book }) => {
     <div className="w-[280px] h-[455px]  rounded-tl-[15px] rounded-tr-[15px] rounded-bl-[10px] rounded-br-[10px]   ">
       <div className="bg-slate-600 w-[280px] h-[280px] rounded-tl-[15px] rounded-tr-[15px]"></div>
       <div className="shadow-10cus w-[280px] h-[175px] rounded-bl-[10px] rounded-br-[10px]">
-        <div className="mx-[20px] mt-[20px] mb-[4px]">
+        <div className="px-[20px] pt-[20px] pb-[4px]">
           <p className="text-[18px] font-medium tracking-[-0.9px] text-center">
             {book.name}
             <br />
